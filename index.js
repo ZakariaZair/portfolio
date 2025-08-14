a94697dd4e56d5ca12018d323b7220404478851c
ZakariaZair <zakaria.zair@polymtl.ca>
2025-08-14 17:09:39 -0400

    fix: import naming

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { timeWriter } from "./helper.ts";
+import { timeWriter } from "./helper.js";
 
-let myName = "Zakaria Zair";
-let titleName = document.querySelector(".self-name").querySelector("h1");
+let myName = "Zakaria Za";
+let nodeName = document.querySelector(".self-name").querySelector("h1");
+timeWriter(nodeName, myName, 100);
 
-timeWriter(titleName, myName, 100);
+let myDesc = "I am a Software Engineer specialized in AI and Data Science.";
+let nodeDesc = document.querySelector(".self-description").querySelector("p");
+timeWriter(nodeDesc, myDesc, 50);
